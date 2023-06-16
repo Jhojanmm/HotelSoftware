7642301586098369ca4c1389ba09b2dcb91a4606
jhojan martinez <jhojanmartinez04@hotmail.com>
2023-06-16 12:09:33 -0500

    starting project

---
+++ nicova/src/main/java/com/nicova/hotel/Nicova.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nicova.hotel;
+
+/**
+ *
+ * @author jhoja
+ */
+public class Nicova {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
